d98f0e0ff593fddfa956ab9e20d992326526198e
carolinamoralese <caromorales1396@gmail.com>
2023-04-12 17:04:00 -0500

    Agrego los primeros cambios del proyecto de validacion de tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+
+const numeroTarjeta = document.getElementById("enviar")
+numeroTarjeta.addEventListener("click", obtenerNumeroTarjeta)
+
+function obtenerNumeroTarjeta (){
+    let numeroIngresado = document.getElementById("numero-tarjeta").value;
+    //console.log(numeroIngresado)
+    //let esValido = validator.isValid(numeroIngresado)
+    //console.log(esValido)
+
+    let numeroEnmascarado = validator.maskify(numeroIngresado)
+    console.log(numeroEnmascarado)
+  
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,25 @@
 const validator = {
-  // ...
+
+  isValid(creditCardNumber){
+    //
+  },
+
+  maskify(creditCardNumber){
+    creditCardNumber = creditCardNumber.toString()
+   let numeroEnmascarado = ""
+    for (let i = 0; i < creditCardNumber.length; i++) {
+    
+      if(i < 12) {
+        numeroEnmascarado = numeroEnmascarado + "#"
+      }else {
+        numeroEnmascarado = numeroEnmascarado + creditCardNumber[i]
+      
+      }
+      
+    }
+    return numeroEnmascarado
+  },
+
 };
 
 export default validator;
