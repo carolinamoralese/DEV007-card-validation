a2ebbc8be0b55d2a106c097f936150f1bb62f946
carolinamoralese <caromorales1396@gmail.com>
2023-04-13 17:16:21 -0500

    agrego modifiaciones del proyecto card validation

--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ numeroTarjeta.addEventListener("click", obtenerNumeroTarjeta)
 
 function obtenerNumeroTarjeta (){
     let numeroIngresado = document.getElementById("numero-tarjeta").value;
+  
+
     //console.log(numeroIngresado)
-    //let esValido = validator.isValid(numeroIngresado)
-    //console.log(esValido)
 
     let numeroEnmascarado = validator.maskify(numeroIngresado)
     console.log(numeroEnmascarado)
   
+    let esValido = validator.isValid(numeroIngresado)
+    console.log(esValido)
+ 
+   //labelCard.innerHTML =  "El número de tarjeta ingresado es <b>¡Válido!</b>";
+   //if(esValido === true) {
+    //labelCard.innerHTML =  "El número de tarjeta ingresado es <b>¡Válido!</b>";
+   // }
+
+
 }
 
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,51 @@
 const validator = {
 
   isValid(creditCardNumber){
-    //
+   
+    //convertir numero de tarjeta en array
+    let arrayNumeroTarjeta = creditCardNumber.split('');
+    console.log(arrayNumeroTarjeta)
+    
+    //convertir array en orden inverso
+    arrayNumeroTarjeta.reverse()
+    console.log(arrayNumeroTarjeta)
+
+    //mirar las posiciones si son pares o no
+    let sumaPar = 0
+    let sumaImpar = 0
+    let sumaTotal = 0
+    for (let i = 0; i < arrayNumeroTarjeta.length; i++) {
+      console.log((i+1) % 2 )
+      if((i+1) % 2 == 0 ) {
+        
+      
+        let numeroMultiplicado = arrayNumeroTarjeta[i] * 2
+       
+        if(numeroMultiplicado >= 10){
+          let stringNumeroMultiplicado = numeroMultiplicado.toString()
+          let arrayNumeroMultiplicado = stringNumeroMultiplicado.split('');
+         
+          sumaPar += parseInt(arrayNumeroMultiplicado[0]) + parseInt(arrayNumeroMultiplicado[1])
+          
+        }
+        
+      }else{
+        sumaImpar += parseInt(arrayNumeroTarjeta[i])
+       
+      }
+      
+    }
+    sumaTotal = sumaPar + sumaImpar
+        
+
+
+    let resultado = sumaTotal % 10
+    console.log(resultado)
+    if(resultado == 0){
+      return true
+    }else{
+      return false
+    }
   },
 
   maskify(creditCardNumber){
